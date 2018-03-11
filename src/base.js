e107f033e1a11f7c0f55685fb2b3610f7398a338
Onildo Aguiar <onildo.aguiar@gmail.com>
2018-03-11 18:11:30 -0300

    Update base.js

--- src/base.js
+++ src/base.js
@@ -1,8 +1,8 @@
 import rebase from 're-base';
 import firebase from 'firebase';
-import firebaseConfig from './config/firebase';
+import FirebaseConfig from './config/firebase';
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(FirebaseConfig);
 
 export const auth = firebaseApp.auth();
 export const providers = { 'facebook': new firebase.auth.FacebookAuthProvider() }
